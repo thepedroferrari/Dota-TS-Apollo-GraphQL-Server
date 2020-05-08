c8ca23227f58541cb5db47f9d8192db27390de7e
Pedro Ferrari <pedro@pedroferrari.com>
2020-05-08 21:29:18 +0200

    Add custom scalar Date

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,8 @@
+import { GraphQLScalarType, Kind } from 'graphql';
 import { IResolvers } from 'graphql-tools';
 import { IEvent } from './interfaces';
 import { mockData } from './mockData';
+
 
 export const resolvers: IResolvers = {
   Query: {
@@ -10,5 +12,24 @@ export const resolvers: IResolvers = {
     event: (obj, { id }: { id: string; }): IEvent | undefined => {
       return mockData.find((event: IEvent) => event.id === Number(id));
     }
-  }
+  },
+  Date: new GraphQLScalarType({
+    name: "Date",
+    description: 'A date in the format of YYYY-mm-dd hh:mm:ss',
+    parseValue(value) {
+      // data from the client, eg. 2019-01-27 12:38:14
+      // I want to store this as a Date
+      return new Date(value);
+    },
+    serialize(value) {
+      // data to be sent to the client, eg 1595829855 (epoch)
+      return new Date(value).getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    }
+  })
 };
